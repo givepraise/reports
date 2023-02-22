4f97de75307fc4fe21bfd23adcf5e0b604239799
kristoferlund <kristofer@fmckl.se>
2023-02-22 12:46:45 +0100

    ...

--- reports/dist-straight-curve-with-ceiling/base.js
+++ reports/dist-straight-curve-with-ceiling/base.js
@@ -120,13 +120,9 @@ export default class BaseReport {
     ;`;
 
     let rows = await this.db.query(sql);
-    return rows;
 
-    if (Array.isArray(rows) && rows.length > 0) {
-      rows = this.filterNoRewardsAddress(rows);
-      rows = this.filterReceivers(rows);
-      return rows;
-    }
-    return;
+    rows = this.filterNoRewardsAddress(rows);
+    rows = this.filterReceivers(rows);
+    return rows;
   }
 }
