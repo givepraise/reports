32cd187bc04a4102de454a3389db137c70ff16e9
kristoferlund <kristofer@fmckl.se>
2023-04-28 16:33:20 +0200

    Fix top praise report bug

--- reports/top-praise/report.js
+++ reports/top-praise/report.js
@@ -16,7 +16,7 @@ export default class Report extends BaseReport {
         gu.username AS giver_users_username, gu.identityEthAddress AS giver_users_identityEthAddress, gu.rewardsEthAddress AS giver_users_rewardsEthAddress, gu.roles AS giver_users_roles, gu.createdAt AS giver_users_createdAt, gu.updatedAt AS giver_users_updatedAt,
         gac.accountId AS giver_useraccounts_accountId, gac.user as giver_useraccounts_user, gac.name AS giver_useraccounts_name, gac.avatarId AS giver_useraccounts_avatarId, gac.platform AS giver_useraccounts_platform, gac.createdAt AS giver_useraccounts_createdAt, gac.updatedAt AS giver_useraccounts_updatedAt,
         ru.username AS receiver_users_username, ru.identityEthAddress AS receiver_users_identityEthAddress, ru.rewardsEthAddress AS receiver_users_rewardsEthAddress, ru.roles AS receiver_users_roles, ru.createdAt AS receiver_users_createdAt, ru.updatedAt AS receiver_users_updatedAt,
-        rac.accountId AS giver_useraccounts_accountId, rac.user as giver_useraccounts_user, rac.name AS receiver_useraccounts_name, rac.avatarId AS receiver_useraccounts_avatarId, rac.platform AS receiver_useraccounts_platform, rac.createdAt AS receiver_useraccounts_createdAt, rac.updatedAt AS receiver_useraccounts_updatedAt
+        rac.accountId AS receiver_useraccounts_accountId, rac.user as receiver_useraccounts_user, rac.name AS receiver_useraccounts_name, rac.avatarId AS receiver_useraccounts_avatarId, rac.platform AS receiver_useraccounts_platform, rac.createdAt AS receiver_useraccounts_createdAt, rac.updatedAt AS receiver_useraccounts_updatedAt
       FROM praises AS p
       LEFT JOIN useraccounts AS gac ON p.giver = gac._id
       LEFT JOIN users AS gu ON gac.user = gu._id
