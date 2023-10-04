59fd579c9b4c1868595b5d4555be87841d52a734
Kristofer <kristofer@fmckl.se>
2023-10-04 16:31:22 +0200

    Include quantifier flag

--- reports/attestations/report.js
+++ reports/attestations/report.js
@@ -15,33 +15,39 @@ export default class Report extends BaseReport {
 
     // SQL Query for receiving scores
     const sqlReceivers = `
-      SELECT
-        ru.username AS users_username, 
-        ru.identityEthAddress AS users_identityEthAddress, 
-        FLOOR(SUM(praises.score)) AS total_received_praise_score
-      FROM praises
-      LEFT JOIN useraccounts AS rac ON praises.receiver = rac._id
-      LEFT JOIN users AS ru ON rac.user = ru._id
-      ${where}
-      GROUP BY ru.username, ru.identityEthAddress
-      ORDER BY total_received_praise_score DESC
-    `;
+    SELECT
+      ru.username AS users_username, 
+      rac.name AS useraccounts_name, 
+      ru.identityEthAddress AS users_identityEthAddress, 
+      FLOOR(SUM(praises.score)) AS total_received_praise_score,
+      MAX(CASE WHEN quantifications.quantifier IS NOT NULL THEN 1 ELSE 0 END) AS is_quantifier
+    FROM praises
+    LEFT JOIN useraccounts AS rac ON praises.receiver = rac._id
+    LEFT JOIN users AS ru ON rac.user = ru._id
+    LEFT JOIN quantifications ON quantifications.quantifier = ru._id
+    ${where}
+    GROUP BY ru.username, ru.identityEthAddress, rac.name
+    ORDER BY total_received_praise_score DESC
+  `;
 
     const rowsReceivers = await this.db.query(sqlReceivers);
 
     // SQL Query for given scores
     const sqlGivers = `
-      SELECT 
-        gu.username AS users_username, 
-        gu.identityEthAddress AS users_identityEthAddress, 
-        FLOOR(SUM(praises.score)) AS total_given_praise_score
-      FROM praises
-      LEFT JOIN useraccounts AS gac ON praises.giver = gac._id
-      LEFT JOIN users AS gu ON gac.user = gu._id
-      ${where}
-      GROUP BY gu.username, gu.identityEthAddress
-      ORDER BY total_given_praise_score DESC
-    `;
+    SELECT 
+      gu.username AS users_username,
+      gac.name AS useraccounts_name, 
+      gu.identityEthAddress AS users_identityEthAddress, 
+      FLOOR(SUM(praises.score)) AS total_given_praise_score,
+      MAX(CASE WHEN quantifications.quantifier IS NOT NULL THEN 1 ELSE 0 END) AS is_quantifier
+    FROM praises
+    LEFT JOIN useraccounts AS gac ON praises.giver = gac._id
+    LEFT JOIN users AS gu ON gac.user = gu._id
+    LEFT JOIN quantifications ON quantifications.quantifier = gu._id
+    ${where}
+    GROUP BY gu.username, gu.identityEthAddress, gac.name
+    ORDER BY total_given_praise_score DESC
+  `;
 
     const rowsGivers = await this.db.query(sqlGivers);
 
@@ -52,6 +58,7 @@ export default class Report extends BaseReport {
       users.set(row.users_username, {
         users_username: row.users_username,
         users_identityEthAddress: row.users_identityEthAddress,
+        useraccounts_name: row.useraccounts_name,
         total_received_praise_score: row.total_received_praise_score ?? 0,
         top_10_receiver: index < 10,
         top_50_receiver: index < 50,
@@ -60,6 +67,7 @@ export default class Report extends BaseReport {
         top_10_giver: false,
         top_50_giver: false,
         top_100_giver: false,
+        quantifier: !!row.is_quantifier,
       });
     });
 
@@ -67,6 +75,7 @@ export default class Report extends BaseReport {
       const existingUser = users.get(row.users_username) || {
         users_username: row.users_username,
         users_identityEthAddress: row.users_identityEthAddress,
+        useraccounts_name: row.useraccounts_name,
         total_received_praise_score: 0,
         top_10_receiver: false,
         top_50_receiver: false,
@@ -78,6 +87,7 @@ export default class Report extends BaseReport {
         top_10_giver: index < 10,
         top_50_giver: index < 50,
         top_100_giver: index < 100,
+        quantifier: existingUser.is_quantifier || !!row.is_quantifier,
       });
     });
 
@@ -89,7 +99,7 @@ export default class Report extends BaseReport {
     users.forEach((user, key) => {
       if (!user.users_identityEthAddress) {
         users.delete(key);
-        this.log(`- ${key}`);
+        this.log(`- ${user.useraccounts_name}`);
         deletedCount++;
       }
     });
